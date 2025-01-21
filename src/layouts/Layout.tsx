d009884489ffb99248a7bbdd3e271ca87f82dab1
Ariel Hurtado <arielhurtado388@gmail.com>
2025-01-21 01:45:52 -0500

    Btns cerra y add favoritos

--- src/layouts/Layout.tsx
+++ src/layouts/Layout.tsx
@@ -1,5 +1,6 @@
 import { Outlet } from "react-router-dom";
 import Header from "../components/Header";
+import Modal from "../components/Modal";
 
 export default function Layout() {
   return (
@@ -9,6 +10,8 @@ export default function Layout() {
       <main className="container md:max-w-screen-xl mx-auto p-6">
         <Outlet />
       </main>
+
+      <Modal />
     </>
   );
 }
