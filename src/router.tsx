bf0abd2bfcc40bc0274cbfe485da8f20564d7fb4
Ariel Hurtado <arielhurtado388@gmail.com>
2025-01-21 23:51:29 -0500

    Mejoras de performance Index

--- src/router.tsx
+++ src/router.tsx
@@ -1,8 +1,8 @@
 import { lazy, Suspense } from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import IndexPage from "./views/IndexPage";
 import Layout from "./layouts/Layout";
 
+const IndexPage = lazy(() => import("./views/IndexPage"));
 const FavoritesPage = lazy(() => import("./views/FavoritesPage"));
 
 export default function AppRouter() {
@@ -10,11 +10,19 @@ export default function AppRouter() {
     <BrowserRouter>
       <Routes>
         <Route element={<Layout />}>
-          <Route path="/" element={<IndexPage />} index />
+          <Route
+            path="/"
+            element={
+              <Suspense fallback="Cargando ...">
+                <IndexPage />
+              </Suspense>
+            }
+            index
+          />
           <Route
             path="/favoritos"
             element={
-              <Suspense fallback="Cargando">
+              <Suspense fallback="Cargando ...">
                 <FavoritesPage />
               </Suspense>
             }
